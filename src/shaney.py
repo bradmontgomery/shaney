124ba6c1a1a86985c194da3f4c3861646a0f7316
Brad Montgomery <brad@bradmontgomery.net>
2015-01-17 11:41:12 -0600

    use print function; remove unnecessary things

--- src/shaney.py
+++ src/shaney.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
+"""
+shaney.py by Greg McFarlane some editing by Joe Strout
+search for "Mark V.  Shaney" on the WWW for more info!
 
-# shaney.py by Greg McFarlane
-# some editing by Joe Strout
-# search for "Mark V.  Shaney" on the WWW for more info!
+"""
+from __future__ import print_function
 
 import sys
 import random
 import string
 
 
-def run(filename=''):
+
+def run(filename='', count=10):
     if filename == '':
         file = open(raw_input('Enter name of a textfile to read: '), 'r')
     else:
         file = open(filename, 'r')
-
-    text = file.read()
-    file.close()
-    words = string.split(text)
 
     end_sentence = []
     data = {}
     prev1 = ''
     prev2 = ''
 
-    for word in words:
+    # Load up a dictionary of data from the input files.
+    for word in string.split(file.read()):
         if prev1 != '' and prev2 != '':
             key = (prev2, prev1)
             if key in data:
@@ -35,22 +35,23 @@ def run(filename=''):
                     end_sentence.append(key)
         prev2 = prev1
         prev1 = word
+    file.close()
 
     if end_sentence == []:
-        print 'Sorry, there are no sentences in the text.'
+        print('Sorry, there are no sentences in the text.')
         return
 
+    # Generate some text.
     key = ()
-    count = 10
-
-    while 1:
+    output = ""
+    while True:
         if key in data:
             word = random.choice(data[key])
-            print word,
+            print(word, end=" ")
 
             key = (key[1], word)
             if key in end_sentence:
-                print "\n\n{0}\n".format("-" * 80)
+                print("\n\n{0}\n".format("-" * 80))
                 count = count - 1
                 key = random.choice(end_sentence)
                 if count <= 0:
@@ -59,13 +60,8 @@ def run(filename=''):
             key = random.choice(end_sentence)
 
 
-# immediate-mode commands, for drag-and-drop or execfile() execution
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         run(sys.argv[1])  # accept a command-line filename
     else:
         run()
-else:
-    print "Module shaney imported."
-    print "To run, type: shaney.run()"
-    print "To reload after changes to the source, type: reload(shaney)"
