23d089f52b1fc9d4ba7409c960d0fedda75989fd
Brad Montgomery <brad@bradmontgomery.net>
2015-02-04 22:28:44 -0600

    improved text pre-processing and split the learning and text generation into two separate pieces

--- src/shaney.py
+++ src/shaney.py
@@ -20,17 +20,25 @@ def read(filename):
     content = open(filename, 'r').read()
     # kill the unicode things (e.g. smart quotes)
     content = content.decode('utf8').encode('ascii', 'ignore')
+    # condense all whitespace chars into a single space
+    content = re.sub('\s+', ' ', content)
     # Remove everything but words, major punctuation, and single quotes
     content = re.sub('[^A-Za-z\.\?\!\' ]+', '', content)
     return string.split(content)
 
 
-def run(filename=None, count=10):
-    if filename is None:
-        filename = raw_input('Enter name of a textfile to read: ')
+def train(filename):
+    """Reads a file, building the data model used to generate text.
+    Returns trained data; a dictionary of the form:
 
-    end_sentence = []
-    data = {}
+        { content: {},
+          endings: [] }
+
+    """
+    endings = []  # Pairs of words that end a sentence
+    data = {}  # Key: Pairs of words, Values: List of words that follow those pairs.
+
+    # and here's a pair of words that we encounter in a text.
     prev1 = ''
     prev2 = ''
 
@@ -43,31 +51,55 @@ def run(filename=None, count=10):
             else:
                 data[key] = [word]
                 if any([prev1.endswith(punct) for punct in ['.', '?', '!']]):
-                    end_sentence.append(key)
+                    endings.append(key)
         prev2 = prev1
         prev1 = word
+    assert endings != [], "Sorry, there are no sentences in the text."
+    return {'content': data, 'endings': endings}
 
-    if end_sentence == []:
-        print('Sorry, there are no sentences in the text.')
-        return
 
-    # Generate some text.
-    key = ()
+def generate(data, count=10):
+    """Given the appropriate data library (dict of 'content' and 'endings' keys),
+    this will generate text strings.
+
+    Returns a list of `count` strings.
+
+    """
     output = ""
+    generated_strings = []
+    key = ()
+
     while True:
-        if key in data:
-            word = random.choice(data[key])
-            print(word, end=" ")
+        if key in data['content']:
+            word = random.choice(data['content'][key])
+            output = "{0}{1} ".format(output, word)
 
             key = (key[1], word)
-            if key in end_sentence:
-                print("\n\n{0}\n".format("-" * 80))
+            if key in data['endings']:
+                generated_strings.append(output)
+                output = ""
+                key = random.choice(data['endings'])
                 count = count - 1
-                key = random.choice(end_sentence)
                 if count <= 0:
                     break
         else:
-            key = random.choice(end_sentence)
+            key = random.choice(data['endings'])
+
+    return generated_strings
+
+
+def run(filename=None, count=10):
+    """Given a path to a file, read it, build a library, and generate 10
+    sentences from it, printing them.
+    """
+
+    if filename is None:
+        filename = raw_input('Enter name of a textfile to read: ')
+
+    data = train(filename)
+    results = generate(data, count)
+    for r in results:
+        print("\n{0}".format(r))
 
 
 if __name__ == '__main__':
