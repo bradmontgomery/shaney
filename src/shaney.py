47a14ca677dd2a6e0fee1f1d40834461594cbdfc
Brad Montgomery <brad@bradmontgomery.net>
2015-02-23 22:34:22 -0600

    added a function to do simple analysis of the text sample size; added lots more output

--- src/shaney.py
+++ src/shaney.py
@@ -18,11 +18,15 @@ def write(msg, verbose=False):
         sys.stdout.write(u"{0}\n".format(msg))
 
 
-def read(filename):
+def read(filename, verbose=False):
     """Reads content from the file, does a little bit of cleanup, and returns
     a list of words.
 
     """
+    # Some informative output (if running in verbose mode)
+    write("-" * 80, verbose)
+    write("Reading File.", verbose)
+
     content = open(filename, 'r').read()
     # kill the unicode things (e.g. smart quotes)
     content = content.decode('utf8').encode('ascii', 'ignore')
@@ -30,7 +34,35 @@ def read(filename):
     content = re.sub('\s+', ' ', content)
     # Remove everything but words, major punctuation, and single quotes
     content = re.sub('[^A-Za-z\.\?\!\' ]+', '', content)
-    return string.split(content)
+    content = string.split(content)
+
+    write("Read {0} lines.".format(len(content)), verbose)
+    return content
+
+
+def analyze_text(data, verbose=True):
+    """Given the dictionary of trained data, print the mean word sample size.
+    Larger numbers yield better results.
+
+    This function assumes you want to print things to stdout.
+
+    """
+
+    size = 0
+    for word_list in data.values():
+        size += len(word_list)
+    avg_sample_size = float(size) / len(data.values())
+
+    write("-" * 80, verbose)
+    write("Average Sample Size: {0}".format(avg_sample_size), verbose)
+    if avg_sample_size < 2:
+        write("--> This is a small sample size, and is probably like a poor "
+              "at generating text.", verbose)
+    if avg_sample_size > 2:
+        write("--> This looks like a decent sample size.", verbose)
+    else:
+        write("--> This looks like a good sample size.", verbose)
+    write("-" * 80, verbose)
 
 
 def train(filename, verbose=False):
@@ -41,7 +73,7 @@ def train(filename, verbose=False):
           endings: [] }
 
     """
-    write("Training...")
+    write("Training...", verbose)
     endings = []  # Pairs of words that end a sentence
     data = {}  # Key: Pairs of words, Values: List of words that follow those pairs.
 
@@ -50,7 +82,7 @@ def train(filename, verbose=False):
     prev2 = ''
 
     # Load up a dictionary of data from the input files.
-    for word in read(filename):
+    for word in read(filename, verbose):
         if prev1 != '' and prev2 != '':
             key = (prev2, prev1)
             if key in data:
@@ -62,16 +94,20 @@ def train(filename, verbose=False):
         prev2 = prev1
         prev1 = word
     assert endings != [], "Sorry, there are no sentences in the text."
+    if verbose:
+        analyze_text(data)
     return {'content': data, 'endings': endings}
 
 
-def generate(data, count=10):
+def generate(data, count=10, verbose=False):
     """Given the appropriate data library (dict of 'content' and 'endings' keys),
     this will generate text strings.
 
     Returns a list of `count` strings.
 
     """
+    write("Generating output:", verbose)
+
     output = ""
     generated_strings = []
     key = ()
@@ -99,14 +135,16 @@ def run(filename=None, count=10, verbose=False):
     """Given a path to a file, read it, build a library, and generate 10
     sentences from it, printing them.
     """
-
+    write("Running in verbose mode.", verbose)
     if filename is None:
         filename = raw_input('Enter name of a textfile to read: ')
 
     data = train(filename, verbose)
     results = generate(data, count, verbose)
     for r in results:
-        write("\n{0}".format(r), verbose)
+        write("\n* {0}".format(r), verbose)
+    write("-" * 80, verbose)
+    return results
 
 
 if __name__ == '__main__':
