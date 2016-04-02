a404f3aeb66fb8a764b8084d31bd05dc2c83375a
Brad Montgomery <brad@bradmontgomery.net>
2016-04-02 18:54:51 -0500

    generalizing the web app

--- src/web.py
+++ src/web.py
@@ -1,30 +1,40 @@
+import sys
+
 from flask import Flask
 from flask import render_template
 from shaney import train, generate
 
 
 app = Flask(__name__)
+shaney_bot = None
 
 
 class Shaney:
-    def __init__(self):
-        self.data = train("/Users/brad/Downloads/markov-corpus/foxnews.txt")
+    def __init__(self, file):
+        self.data = train(file)
 
     def quote(self):
-        quotes = generate(self.data, count=1, verbose=False)
         try:
+            quotes = generate(self.data, count=1, verbose=False)
             quote = quotes[0].strip()
         except:
             quote = "oops. I failed to generate a quote. Try again?"
         return quote
 
-shaney_bot = Shaney()
-
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', quote=shaney_bot.quote())
+    if shaney_bot:
+        quote = shaney_bot.quote()
+    else:
+        quote = "oops."
+    return render_template('index.html', quote=quote)
 
 
 if __name__ == '__main__':
-    app.run()
+    filename = None if len(sys.argv) < 2 else sys.argv[1]
+    if filename is not None:
+        shaney_bot = Shaney(filename)
+        app.run()
+    else:
+        print("\n\nUsage: python web.py /path/to/content.txt\n\n")
